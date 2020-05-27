2e39e5af2608544a2389450d76d710b646698a14
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-27 10:05:57 +0200

    Creazione lista finale (pushata oggi per mancanza internet di ieri)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var cognomeUtente = prompt("Digitare il proprio cognome")
 var cognomeValido = false;
 var databaseCognomi = ["Bianchi","Rossi","Duzioni","Balsano","Verdi"]
+var lista = document.getElementById('lista');
 
 // Verifica dei caratteri del cognome
 while ((!isNaN(cognomeUtente)) || (cognomeUtente=="")) {
@@ -13,10 +14,14 @@ cognomeValido = true;
 // Processo di inserimento cognome nel database
 if (cognomeValido) {
   cognomeDefinitivo = cognomeUtente.toUpperCase();
-  console.log(cognomeDefinitivo)
   databaseCognomi.push(cognomeDefinitivo);
   databaseCognomi.sort();
 }
 // Messaggi di conferma
 alert("La lista dei cognomi è: " + databaseCognomi)
 alert("Il tuo cognome è il " + (databaseCognomi.indexOf(cognomeDefinitivo) + 1) + "°");
+
+for (var i = 0; i < databaseCognomi.length; i++) {
+  var listaCorrente = lista.innerHTML;
+  lista.innerHTML = listaCorrente + "<li>" + databaseCognomi[i] + "</li>";
+}
